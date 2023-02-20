41a5e36adff728d263b562996747e640fa838d68
ChaosHarmony <harmonie.lebrun1@gmail.com>
2023-02-20 16:09:06 +0100

    added NET class which is a collection of Floor

--- floor.py
+++ floor.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Floor():
 
     def __init__(self, id: int, level: int) -> None:
@@ -16,3 +19,22 @@ class Floor():
             print("Child is not of Floor type")
         else:
             self.childs.append(child_floor)
+
+
+class NET():
+
+    def __init__(self, floors_list) -> None:
+        """
+        floors_list is an array like with floors as input
+        NET class generalize floor class
+        """
+        self.n_floors = len(floors_list)
+        self.floors = floors_list
+        self.depth = max([floors_list[i].level for i in range(self.n_floors)])
+        self.id = [floors_list[i].id for i in range(self.n_floors)]
+
+    def id_to_index(self, floor_id):
+        return np.where(floor_id == self.id)
+
+    def fill_floor(self, floor_id, data):
+        self.floors[self.id_to_index(floor_id)].inside = data
